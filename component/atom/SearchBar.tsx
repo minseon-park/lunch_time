c2c837c131a46612ef608a11c0ab4b8181282d37
minseon <minseonpark00@gmail.com>
2022-02-11 16:20:45 +0900

    Modified Searchbar

--- component/atom/SearchBar.tsx
+++ component/atom/SearchBar.tsx
@@ -1,65 +1,67 @@
 import React , { useState } from "react";
 import styled from "styled-components";
+import Image, { ImageProps } from "next/image";
 import SearchIcon from "../../images/SearchIcon.png"
+import { useForm, SubmitHandler } from "react-hook-form";
 
-//TODO: 재사용성 reusable 한 search bar 만들기 (placeholder 받아오기 )
- export const SearchBar = () => {
-    const [text, setText] = useState("");
-    const handleChange = (event: any) => {
-        setText(event.target.value);
-    };
-    const clearInput = () => {
-        setText("");
-    };
+type Inputs = {
+    searchText: string,
+}
+
+export const SearchBar = () => {
+    const {register, handleSubmit} = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
     return (
-        <form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <StyledInput className={"inputWithIcon"}>
                 <Input 
-                    type="text"
-                    value={text}
-                    onChange={handleChange}
+                    {...register("searchText", {required: true})}
                     placeholder= "지금 안 땡기는 음식 찾기"
-                    onSubmit={e => { e.preventDefault(); }}
                 />
-                <button className="right-icon" onClick={clearInput}>
-                </button>
+                <Button type="submit">
+                    <Image
+                    src={SearchIcon}
+                    alt="image"
+                    />
+                </Button>
             </StyledInput>
-        </form>
+        </Form>
     );
 };
 
+const Form = styled.form`
+    width: 55rem;
+    height: 4rem;
+    padding-left: 2rem;
+    color: 494949;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+    outline: none;
+    background: #F0F5F9;
+`;
 
-//TODO: img buttton 만들기
 const StyledInput = styled.div`
-    &.inputWithIcon {
-        position: relative;
-    }
     display: flex;
-    button.right-icon {
-        margin-right: 1rem;
-        width: 2rem;
-        height: 2rem;
-        background-image: url(${SearchIcon.src});
-        background: none;
-        // border: none;
-        position: absolute;
-        right: 5px;
-        top: 50%;
-        transform: translateY(-50%);
-
-    };
 `;
 const Input = styled.input`
-  width: 55rem;
-  height: 4rem;
-  font-size: 1rem;
-  padding-left: 2rem;
-  color: 494949;
-  border: none;
-  border-radius: 10px;
-  box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
-  outline: none;
-  background: #F0F5F9;
-  cursor: pointer;
-  }
+    border: none;
+    background: none;
+    font-size: 1rem;
+    color: 494949;
+    width: 51rem;
+    height: 4rem;
+    outline: none;
+`;
+
+const Button = styled.button`
+    margin-right: 1rem;
+    width: 2rem;
+    height: 4rem;
+    border: none;
+    background: none;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    outline: none;
 `;
